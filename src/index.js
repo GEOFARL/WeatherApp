5878887fcb8a453474dcbe83c29ff37b245ab532
Maxim <toper.one111@gmail.com>
2023-01-23 13:17:15 +0200

    Load data and create a box for today forecast
    - Created a box for showing a forecast for 24 hours ahead
    - Loaded extracted data to the website

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import './template.html';
 import './sass/main.scss';
 import { extractDataNow, extractDataForecast } from './modules/extractData';
 
+function loadCurrentWeather(data) {
+  const currentTemp = document.querySelector('.current-weather__temperature');
+  const currentCity = document.querySelector('.current-weather__info-box__city');
+  const currentDate = document.querySelector('.current-weather__info-box__date');
+  const currentIcon = document.querySelector('.current-weather__icon');
+
+  currentTemp.innerHTML = `${data.temperature}&deg;`;
+  currentCity.innerHTML = data.city;
+  currentDate.innerHTML = data.date;
+  currentIcon.src = data.icon;
+}
+
+function loadForecastToday(forecast) {
+  const todayCards = [...document.querySelectorAll('.today-forecast__card')];
+  todayCards.forEach((card, index) => {
+    const time = card.querySelector('.today-forecast__card__time');
+    const icon = card.querySelector('.today-forecast__card__icon');
+    const temp = card.querySelector('.today-forecast__card__temperature');
+    const data = forecast.today[index];
+
+    time.innerText = data.time;
+    icon.src = data.icon;
+    temp.innerHTML = `${data.temperature}&deg;C`;
+  });
+}
+
 async function retrieveWeatherData(location) {
   try {
     const [responseForecast, responseToday] = await Promise.all([
@@ -14,6 +40,10 @@ async function retrieveWeatherData(location) {
     ]);
     const today = extractDataNow(jsonN);
     const forecast = extractDataForecast(jsonF);
+
+    loadCurrentWeather(today);
+    loadForecastToday(forecast);
+
     console.log(today);
     console.log(forecast);
     document.querySelector('img').src = today.icon;
@@ -22,4 +52,4 @@ async function retrieveWeatherData(location) {
   }
 }
 
-retrieveWeatherData('Kyiv');
+retrieveWeatherData('California');
